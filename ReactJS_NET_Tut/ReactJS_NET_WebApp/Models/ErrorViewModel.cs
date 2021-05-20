c67ae9b10b559d69252c2a82b1f10545013efe81
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-05-21 00:53:21 +1000

    Набросал основу для примера использования реакта в ASP.NET Core

---
+++ ReactJS_NET_Tut/ReactJS_NET_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReactJS_NET_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
